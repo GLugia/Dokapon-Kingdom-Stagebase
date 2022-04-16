369ce11522c9b51a4122540d999babcabb8e23fe
GLugia <glugia@hotmail.com>
2022-04-16 14:00:05 -0400

    Commented DataWriter

--- data/DataWriter.cs
+++ data/DataWriter.cs
@@ -8,12 +8,33 @@ namespace CharaReader.data
 {
 	public class DataWriter
 	{
+		/// <summary>
+		/// The file this buffer will write to.
+		/// </summary>
 		private string _file;
+		/// <summary>
+		/// The buffer.
+		/// </summary>
 		private byte[] _data;
+		/// <summary>
+		/// A public, readonly instance of the buffer.
+		/// </summary>
 		public ReadOnlySpan<byte> data => _data.AsSpan();
+		/// <summary>
+		/// The current offset of the buffer.
+		/// </summary>
 		public int offset;
+		/// <summary>
+		/// The current offset of the buffer in hex.
+		/// </summary>
 		public string offset_hex => offset.ToHexString();
+		/// <summary>
+		/// The current length of the buffer. Defaults to -1 if the buffer is <see langword="null"/>.
+		/// </summary>
 		public int length => _data?.Length ?? -1;
+		/// <summary>
+		/// A list of named offsets reserved for pointer use.
+		/// </summary>
 		private Dictionary<string, Pointer> reserved_offsets;
 
 		/// <summary>
@@ -53,7 +74,7 @@ namespace CharaReader.data
 		}
 
 		/// <summary>
-		/// Handles writing primitive objects that may not be immediately known. ie dynamics.
+		/// Handles writing primitive objects that may not be immediately known. ie. dynamics.
 		/// </summary>
 		/// <param name="val">The value to write.</param>
 		/// <param name="alignment">The alignment of the data.</param>
@@ -62,82 +83,24 @@ namespace CharaReader.data
 		{
 			switch (val)
 			{
-				case bool:
-					{
-						Write((bool)val);
-						break;
-					}
-				case char:
-					{
-						Write((char)val);
-						break;
-					}
-				case char[]:
-					{
-						Write((char[])val);
-						break;
-					}
-				case string:
-					{
-						Write((string)val, alignment);
-						break;
-					}
-				case sbyte:
-					{
-						Write((sbyte)val);
-						break;
-					}
-				case byte:
-					{
-						Write((byte)val);
-						break;
-					}
-				case byte[]:
-					{
-						Write((byte[])val);
-						break;
-					}
-				case short:
-					{
-						Write((short)val);
-						break;
-					}
-				case ushort:
-					{
-						Write((ushort)val);
-						break;
-					}
-				case int:
-					{
-						Write((int)val);
-						break;
-					}
-				case uint:
-					{
-						Write((uint)val);
-						break;
-					}
-				case long:
-					{
-						Write((long)val);
-						break;
-					}
-				case ulong:
-					{
-						Write((ulong)val);
-						break;
-					}
-				case float:
-					{
-						Write((float)val);
-						break;
-					}
+				case bool: Write((bool)val); break;
+				case char: Write((char)val); break;
+				case char[]: Write((char[])val); break;
+				case string: Write((string)val, alignment); break;
+				case sbyte: Write((sbyte)val); break;
+				case byte: Write((byte)val); break;
+				case byte[]: Write((byte[])val); break;
+				case short: Write((short)val); break;
+				case ushort: Write((ushort)val); break;
+				case int: Write((int)val); break;
+				case uint: Write((uint)val); break;
+				case long: Write((long)val); break;
+				case ulong: Write((ulong)val); break;
+				case float: Write((float)val); break;
+				case double: Write((double)val); break;
 				// if it's null or unhandled, throw an error
 				case null:
-				default:
-					{
-						throw new Exception($"Unhandled type: {val.GetType()}::{val}");
-					}
+				default: throw new Exception($"Unhandled type: {val.GetType()}::{val}");
 			}
 		}
 
@@ -245,20 +208,9 @@ namespace CharaReader.data
 		}
 
 		/// <summary>
-		/// Writes a span of 
+		/// Writes an s16 to the buffer.
 		/// </summary>
-		/// <typeparam name="T"></typeparam>
-		/// <param name="val"></param>
-		public void Write<T>(Span<T> val)
-		{
-			if (offset + val.Length > _data.Length - 1)
-			{
-				Array.Resize(ref _data, offset + val.Length + 1);
-			}
-			val.ToArray().CopyTo(_data, offset);
-			offset += val.Length;
-		}
-
+		/// <param name="val">The value to write to the buffer.</param>
 		public void Write(short val)
 		{
 			byte[] data = BitConverter.GetBytes(val);
@@ -270,6 +222,10 @@ namespace CharaReader.data
 			offset += data.Length;
 		}
 
+		/// <summary>
+		/// Writes a u16 to the buffer.
+		/// </summary>
+		/// <param name="val">The value to write to the buffer.</param>
 		public void Write(ushort val)
 		{
 			byte[] data = BitConverter.GetBytes(val);
@@ -281,6 +237,10 @@ namespace CharaReader.data
 			offset += data.Length;
 		}
 
+		/// <summary>
+		/// Writes an s32 to the buffer.
+		/// </summary>
+		/// <param name="val">The value to write to the buffer.</param>
 		public void Write(int val)
 		{
 			byte[] data = BitConverter.GetBytes(val);
@@ -292,6 +252,10 @@ namespace CharaReader.data
 			offset += data.Length;
 		}
 
+		/// <summary>
+		/// Writes a u32 to the buffer.
+		/// </summary>
+		/// <param name="val"></param>
 		public void Write(uint val)
 		{
 			byte[] data = BitConverter.GetBytes(val);
@@ -303,6 +267,10 @@ namespace CharaReader.data
 			offset += data.Length;
 		}
 
+		/// <summary>
+		/// Writes an s64 to the buffer.
+		/// </summary>
+		/// <param name="val">The value to write to the buffer.</param>
 		public void Write(long val)
 		{
 			byte[] data = BitConverter.GetBytes(val);
@@ -314,6 +282,10 @@ namespace CharaReader.data
 			offset += data.Length;
 		}
 
+		/// <summary>
+		/// Writes a u64 to the buffer.
+		/// </summary>
+		/// <param name="val">The value to write to the buffer.</param>
 		public void Write(ulong val)
 		{
 			byte[] data = BitConverter.GetBytes(val);
@@ -325,6 +297,10 @@ namespace CharaReader.data
 			offset += data.Length;
 		}
 
+		/// <summary>
+		/// Writes an f32 to the buffer.
+		/// </summary>
+		/// <param name="val">The value to write to the buffer.</param>
 		public void Write(float val)
 		{
 			byte[] data = BitConverter.GetBytes(val);
@@ -336,104 +312,149 @@ namespace CharaReader.data
 			offset += data.Length;
 		}
 
-		public void WriteStruct<T>(T obj) where T : struct
-		{
-			if (obj.Equals(default))
-			{
-				return;
-			}
-			TypedReference tref = __makeref(obj);
-			object val;
-			foreach (FieldInfo field in obj.GetType().GetFields())
-			{
-				val = field.GetValueDirect(tref);
-				if (val != null)
-				{
-					Write(val);
-				}
-			}
-		}
+		/// <summary>
+		/// Writes an f64 to the buffer.
+		/// </summary>
+		/// <param name="val">The value to write to the buffer.</param>
+		public void Write(double val)
+        {
+			byte[] data = BitConverter.GetBytes(val);
+			if (offset + data.Length > _data.Length - 1)
+            {
+				Array.Resize(ref _data, offset + data.Length + 1);
+            }
+			data.CopyTo(_data, offset);
+			offset += data.Length;
+        }
 
+		/// <summary>
+		/// Writes an array of structs.
+		/// </summary>
+		/// <typeparam name="T">The type of struct to write.</typeparam>
+		/// <param name="id">The id of this section.</param>
+		/// <param name="objs">The array of structs to write.</param>
 		public void WriteStructs<T>(int id, in T[] objs) where T : struct
 		{
+			// if the object given is null, return early
 			if (objs == null)
 			{
 				return;
 			}
+			// initialize a typed reference
 			TypedReference tref;
+			// iterate over the items in this array
 			for (int i = 0; i < objs.Length; i++)
 			{
+				// if it is a default instance of this struct, skip it
 				if (objs[i].Equals(default(T)))
 				{
 					continue;
 				}
+				// make a reference to this struct
 				tref = __makeref(objs[i]);
+				// write the id
 				Write(id);
+				// iterate over the fields in this struct
 				foreach (FieldInfo field in objs[i].GetType().GetFields())
 				{
+					// write the dynamic value of this field
 					Write(field.GetValueDirect(tref));
 				}
 			}
 		}
 
+		/// <summary>
+		/// Writes a double array of structs.
+		/// </summary>
+		/// <typeparam name="T">The type of struct to write.</typeparam>
+		/// <param name="id">The id of this section.</param>
+		/// <param name="objs">The array of structs to write.</param>
 		public void WriteStructs<T>(int id, in T[][] objs) where T : struct
 		{
+			// if the object given is null, return early
 			if (objs == null)
 			{
 				return;
 			}
+			// initialize a typed reference
 			TypedReference tref;
+			// iterate over the double array
 			for (int i = 0; i < objs.Length; i++)
 			{
+				// if the array at this index is null, skip it
 				if (objs[i] == null)
 				{
 					continue;
 				}
+				// iterate over the items in this array
 				for (int j = 0; j < objs[i].Length; j++)
 				{
+					// if it is a default instance of this struct, skip it
 					if (objs[i][j].Equals(default(T)))
 					{
 						continue;
 					}
+					// make a reference to this struct
 					tref = __makeref(objs[i][j]);
+					// write the id
 					Write(id);
+					// iterate over the fields in this struct
 					foreach (FieldInfo field in objs[i][j].GetType().GetFields())
 					{
+						// write the dynamic value of this field
 						Write(field.GetValueDirect(tref));
 					}
 				}
 			}
 		}
 
+		/// <summary>
+		/// Writes a triple array of structs.
+		/// </summary>
+		/// <typeparam name="T">The type of struct to write.</typeparam>
+		/// <param name="id">The id of this section.</param>
+		/// <param name="objs">The array of structs to write.</param>
 		public void WriteStructs<T>(int id, in T[][][] objs) where T : struct
 		{
+			// if the object given is null, return early
 			if (objs == null)
 			{
 				return;
 			}
+			// initialize a typed reference
 			TypedReference tref;
+			// iterate over the triple array
 			for (int i = 0; i < objs.Length; i++)
 			{
+				// if the double array at this index is null, skip it
 				if (objs[i] == null)
 				{
 					continue;
 				}
+				// iterate over the double array
 				for (int j = 0; j < objs[i].Length; j++)
 				{
+					// if the array at this index is null, skip it
 					if (objs[i][j] == null)
 					{
 						continue;
 					}
+					// iterate over the items in this array
 					for (int k = 0; k < objs[i][j].Length; k++)
 					{
+						// if it is a default instance of this struct, skip it
 						if (objs[i][j][k].Equals(default(T)))
 						{
 							continue;
 						}
+						// make a reference to this struct
 						tref = __makeref(objs[i][j][k]);
+						// write the id
 						Write(id);
+						// iterate over the fields in this struct
 						foreach (FieldInfo field in objs[i][j][k].GetType().GetFields())
 						{
+							// write the dynamic value of this field
 							Write(field.GetValueDirect(tref));
 						}
 					}
@@ -441,29 +462,48 @@ namespace CharaReader.data
 			}
 		}
 
+		/// <summary>
+		/// Writes the current offset to all reserved pointers starting with <paramref name="base_name"/> and ending with
+		/// <paramref name="ending"/>. This is not the best way to do this but it is the easiest. This only writes one
+		/// offset per pointer before removing it from the list.
+		/// </summary>
+		/// <param name="base_name">A constant string used in the start of reserved pointers' names.</param>
+		/// <param name="ending">Any object that a reserved pointer's name can end with.</param>
 		public void WriteAllPointers(string base_name, object ending) // ending can be an int offset or 'end'
 		{
+			// allocate a new list of strings
 			List<string> keys_to_remove = new();
+			// initialize an s32 value
 			int old_offset;
+			// iterate the currently reserved offsets
 			foreach ((string key, Pointer ptr) in reserved_offsets)
 			{
+				// if the current object's key contains the given parameters
 				if (key.StartsWith($"{base_name}_") && key.EndsWith($"_{ending}"))
 				{
+					// mark the current offset
 					old_offset = offset;
+					// jump to the pointer's offset
 					offset = ptr.offset;
+					// write the marked offset
 					Write(old_offset);
+					// save this pointer's key
 					keys_to_remove.Add(key);
+					// jump back to the original offset
 					offset = old_offset;
 				}
 			}
+			// iterate over the saved keys
 			foreach (string key in keys_to_remove)
 			{
+				// remove them from the reserved offsets
 				reserved_offsets.Remove(key);
 			}
 		}
 
 		/// <summary>
 		/// Handles writing to all pointers that reference an offset comparative to the origin offset of the byte array.
+		/// This is used when a 0x03 object contains multiple <see cref="Description"/> objects.
 		/// </summary>
 		/// <param name="base_name">The base name of all pointers referencing this Description.</param>
 		/// <param name="description">The description to write.</param>
@@ -483,50 +523,69 @@ namespace CharaReader.data
 		}
 
 		/// <summary>
-		/// Writes an array of <see cref="Description"/>s separated by <paramref name="separator"/> to the buffer.
+		/// Writes an array of <see cref="Description"/>s by scanning through the given <paramref name="descriptions"/> array.
 		/// </summary>
 		/// <param name="base_name">The start of the names of pointers refering to this array of <see cref="Description"/>s</param>
 		/// <param name="descriptions">The <see cref="Description"/> array to write to the buffer.</param>
 		/// <param name="separator">The primitive value to write between each object. (ie. 0xFF or '\0')</param>
 		public void WriteDescriptions(string base_name, Description[] descriptions, dynamic separator)
 		{
+			// initialize an array of bytes
 			byte[] total = Array.Empty<byte>();
+			// get the size of the dynamic separator parameter
 			int separator_size = Utils.Size(separator);
+			// iterate through the descriptions array
 			for (int i = 0; i < descriptions.Length; i++)
 			{
+				// if the description in this index is null or empty, skip it
 				if (descriptions[i] == null || (descriptions[i] != null && (descriptions[i].ptrs.Length == 0 || descriptions[i].description.Length == 0)))
 				{
 					continue;
 				}
+				// initialize a value to remember the last index in the 'total' array
 				int index = 0;
+				// iterate through the pointers in the current description
 				for (int j = 0; j < descriptions[i].ptrs.Length; j++)
 				{
+					// find the end of the data in the current description
 					int end;
 					for (end = index + 1; end + separator_size < descriptions[i].description.Length; end++)
 					{
-						dynamic value = Utils.DynamicPeek(descriptions[i].description, end, separator);
-						if (value == separator)
+						// if the dynamic value at the current end index is the target separator
+						if (Utils.DynamicPeek(descriptions[i].description, end, separator) == separator)
 						{
+							// push the index to include the separator
 							end += separator_size;
+							// then stop scanning
 							break;
 						}
 					}
+					// if the sum of the end index and the separator index is larger than the length of the current description
 					if (end + separator_size >= descriptions[i].description.Length)
 					{
+						// set it to the length
 						end = descriptions[i].description.Length;
 					}
+					// take the bytes from the current index to the end index
 					byte[] data = descriptions[i].description[index..end];
+					// if the sum of the current pointer and the length of the previous array is more than the 'total' array can hold
 					if (descriptions[i].ptrs[j] + data.Length > total.Length - 1)
 					{
+						// resize it to fit
 						Array.Resize(ref total, descriptions[i].ptrs[j] + data.Length);
 					}
+					// copy the 'data' array's contents to the 'total' array starting at the current pointer's index
 					data.CopyTo(total, descriptions[i].ptrs[j]);
+					// set the next index
 					index = end;
 				}
 			}
+			// once all descriptions are accounted for
 			for (int i = 0; i < total.Length; i++)
 			{
+				// write any and all reserved pointers that point to this index
 				WriteAllPointers(base_name, i);
+				// then write the byte at this index
 				Write(total[i]);
 			}
 		}
@@ -664,8 +723,8 @@ namespace CharaReader.data
 		}
 
 		/// <summary>
-		/// Writes the <paramref name="id"/> and <see cref="offset"/> to the target named pointer's next available index. Pointers reserved should reserve 2
-		/// indexes each before using this method.
+		/// Writes the <paramref name="id"/> and <see cref="offset"/> to the target named pointer's next available index.
+		/// Pointers reserved should reserve 2 indexes each before using this method.
 		/// </summary>
 		/// <param name="name">The name of the pointer to write to.</param>
 		/// <param name="id">The id to give this pointer.</param>
